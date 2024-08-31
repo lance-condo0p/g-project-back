f04d36f82d542ab6c7a3cfb565c1f23c0ffc2c93
Grigory Gagarinov <ggagarinov@openintegration.inc>
2024-08-31 19:14:38 +0300

    Minor fix - removed excess code

--- src/main/kotlin/com/example/plugins/server/Routing.kt
+++ src/main/kotlin/com/example/plugins/server/Routing.kt
@@ -3,24 +3,12 @@ package com.example.plugins.server
 import com.example.adapters.AdapterType
 import com.example.routes.*
 import io.ktor.client.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
-import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.plugins.openapi.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting(client: HttpClient, aiAdapterType: AdapterType) {
-    client.config {
-        install(Logging) {
-            // TODO: should be based on logback config
-            level = LogLevel.ALL
-        }
-        install(ContentNegotiation) {
-            jackson()
-        }
-    }
     routing {
         authenticate("auth-basic") {
             transcribeRequest(client, aiAdapterType)
